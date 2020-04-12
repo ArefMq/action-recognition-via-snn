fb4b3f2a86fcf155171a1dfe9faf61a87b2d6249
Aref Moqadam Mehr <arefmq@cafebazaar.ir>
2020-04-12 14:06:46 +0430

    minor bug fix

--- scnn/__init__.py
+++ scnn/__init__.py
@@ -10,6 +10,7 @@ from .readout import ReadoutLayer
 from .heaviside import SurrogateHeaviside
 
 
+
 class SNN(torch.nn.Module):
     def __init__(self, spike_fn=None, device=None, dtype=None, time_expector=None, notifier=None):
         super(SNN, self).__init__()
@@ -24,6 +25,7 @@ class SNN(torch.nn.Module):
             self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         else:
             self.device = device
+        self.to(device, dtype)
 
     def get_trainable_parameters(self):
         res = []
@@ -87,13 +89,19 @@ class SNN(torch.nn.Module):
     def compile(self):
         self.layers = torch.nn.ModuleList(self.layers)
 
-    def forward(self, x):
+    def predict(self, x):
+        if isinstance(x, np.ndarray):
+            x = torch.from_numpy(x)
         shp = x.shape
         if self.layers[0].IS_CONV:
             x = x.view(shp[0], 1, shp[1], shp[2], shp[3])
         else:
             x = x.view(shp[0], shp[1], shp[2] * shp[3])
+        x = x.to(self.device, self.dtype)
 
+        return self.forward(x)
+
+    def forward(self, x):
         for l in self.layers:
             x = l(x)
         return x
@@ -133,9 +141,6 @@ class SNN(torch.nn.Module):
             for x_batch, y_batch in data_loader('train'):
                 dataset_counter += 1
                 self.print_progress('Epoch: %d' % epoch, dataset_counter / dataset_size, width=60)
-
-                x_batch = torch.from_numpy(x_batch).to(self.device, self.dtype)  # FIXME
-                y_batch = torch.from_numpy(y_batch.astype(np.long)).to(self.device)  # FIXME
                 l, n = self.batch_step(loss_func, x_batch, y_batch, optimizer)
                 losses.append(l)
                 nums.append(n)
@@ -147,8 +152,6 @@ class SNN(torch.nn.Module):
                 losses = []
                 nums = []
                 for x_batch, y_batch in data_loader('test'):
-                    x_batch = torch.from_numpy(x_batch).to(self.device, self.dtype)  # FIXME
-                    y_batch = torch.from_numpy(y_batch.astype(np.long)).to(self.device)  # FIXME
                     l, n = self.batch_step(loss_func, x_batch, y_batch)
                     losses.append(l)
                     nums.append(n)
@@ -166,8 +169,9 @@ class SNN(torch.nn.Module):
 
     def batch_step(self, loss_func, xb, yb, opt=None):
         log_softmax_fn = torch.nn.LogSoftmax(dim=1)  # TODO: investigate this
+        y_batch = torch.from_numpy(yb.astype(np.long)).to(self.device)
 
-        y_pred = self.forward(xb)
+        y_pred = self.predict(xb)
         log_y_pred = log_softmax_fn(y_pred)
         loss = loss_func(log_y_pred, yb)
 
@@ -184,9 +188,8 @@ class SNN(torch.nn.Module):
         accs = []
         with torch.no_grad():
             for x_batch, y_batch in data_dl:
-                x_batch = torch.from_numpy(x_batch).to(self.device, self.dtype)
+                output = self.predict(x_batch)
                 y_batch = torch.from_numpy(y_batch.astype(np.long))
-                output = self.forward(x_batch)
                 _, am = torch.max(output, 1)  # argmax over output units
                 tmp = np.mean((y_batch == am).detach().numpy())  # compare to labels
                 accs.append(tmp)
@@ -195,3 +198,4 @@ class SNN(torch.nn.Module):
     @staticmethod
     def print_progress(msg, value, width=80, a='=', b='>', c='.'):
         print('\r%s [%s%s%s] %d%%' % (msg, a*int(value*width), b, c*int((1.-value)*width), value*100), end='')
+
