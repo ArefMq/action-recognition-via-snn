36dcc07f767c94dc3d2767ba4ce9c521f68e105d
Aref Moqadam Mehr <arefmq@cafebazaar.ir>
2020-05-04 17:12:46 +0430

    update dot py code

--- S-CNN-DVS.py
+++ S-CNN-DVS.py
@@ -1,8 +1,22 @@
-
+#!/usr/bin/env python
 # coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import torch
+
+# In[ ]:
+
+
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+SAVE_PLOTS = True
+
+
+# In[2]:
 
 
 from utils import plot_spikes_in_time, print_and_plot_accuracy_metrics, plot_metrics
@@ -14,13 +28,22 @@ from tools.time_expector import TimeExpector
 time_expector = TimeExpector()
 
 
+# In[3]:
+
+
 batch_size = 4
-nb_epochs = 20
+nb_epochs = 4
 
 # device = torch.device("cpu")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float
 
+# test_run = True
+# if test_run:
+#     print('[WARNING] : This is test run.')
+
+
+# In[4]:
 
 # FIXME
 my_laptop = False
@@ -37,7 +60,7 @@ def load_data(trail):
         max_augmentation = 1
         augmentation = False
     else:
-        max_augmentation = 3 if trail == 'train' else 1
+        max_augmentation = 2 if trail == 'train' else 1
         augmentation = True
     
     trail = trail.replace('acc_', '')
@@ -51,6 +74,53 @@ def load_data(trail):
         max_augmentation=max_augmentation,
         frame=60
     )
+
+# original_size = 64
+# for __xb, __yb in batchify(
+#     'test',
+#     DATASET_FOLDER_PATH,
+#     CACHE_FOLDER_PATH,
+#     condition_limit=['natural'],
+#     batch_size=original_size,
+#     augmentation=False,
+#     max_augmentation=1,
+#     frame=60
+# ):
+#     break
+
+# _hist = {i:0 for i in range(12)}
+# for i in __yb:
+#     _hist[i] += 1
+# max_value = max(_hist.values())
+
+# aug_xb = []
+# aug_yb = []
+# for i in range(12):
+#     idx = np.where(__yb == i)[0][0]
+#     to_add = max_value - _hist[i]
+#     for _ in range(to_add):
+#         aug_yb.append(i)
+#         aug_xb.append(__xb[idx, :, :, :])
+
+# # print(__xb.shape)
+# # print(__yb.shape)
+# __yb = np.concatenate([__yb, np.array(aug_yb)])
+# __xb = np.concatenate([__xb, np.array(aug_xb)])
+# print(__xb.shape)
+# print(__yb.shape)
+
+
+# def load_data(trail):
+#     begin = 0
+#     end = begin + batch_size
+#     while end <= __yb.shape[0]:
+#         yield __xb[begin:end, :, :, :], __yb[begin:end]
+#         begin = end
+#         end = begin + batch_size
+
+
+# In[5]:
+
 
 # calculate train dataset size
 train_hist = {i:0 for i in range(12)}
@@ -72,348 +142,41 @@ for x_batch, y_batch in load_data('test'):
         print('\rpre-processing dataset: %d' % dataset_size[1], end='')
 print('\rpre-processing dataset: %d' % dataset_size[1])
 
-
-from scnn.default_configs import *
-
-
-class MyConv3D(torch.nn.Module):
-    IS_CONV = True
-    IS_SPIKING = True
-    HAS_PARAM = True
-
-    def __init__(self, input_shape, input_channels, output_shape=None,
-                 output_channels=1, kernel_size=3, dilation=1,
-                 spike_fn=None, w_init_mean=W_INIT_MEAN, w_init_std=W_INIT_STD, recurrent=False,
-                 lateral_connections=True,
-                 eps=EPSILON, stride=(1, 1, 1), flatten_output=False):
-
-        super(MyConv3D, self).__init__()
-
-        self.kernel_size = np.array(kernel_size)
-        self.dilation = np.array(dilation)
-        self.stride = np.array(stride)
-        self.input_channels = input_channels
-        self.input_shape = input_shape
-
-        self.output_channels = output_channels
-        self.output_shape = output_shape if output_shape is not None else input_shape
-        self.spike_fn = spike_fn
-        self.recurrent = recurrent
-        self.lateral_connections = lateral_connections
-        self.eps = eps
-
-        self.flatten_output = flatten_output
-
-        self.w_init_mean = w_init_mean
-        self.w_init_std = w_init_std
-
-        self.w = torch.nn.Parameter(torch.empty((output_channels, input_channels, *kernel_size)), requires_grad=True)
-        if recurrent:
-            self.v = torch.nn.Parameter(torch.empty((output_channels, output_channels)), requires_grad=True)
-        self.beta = torch.nn.Parameter(torch.empty(1), requires_grad=True)
-        self.b = torch.nn.Parameter(torch.empty(output_channels), requires_grad=True)
-
-        self.reset_parameters()
-        self.clamp()
-
-        self.spk_rec_hist = None
-        self.mem_rec_hist = None
-        self.training = True
-
-#         # TODO : check this
-#         tau_mem = 10e-3
-#         tau_syn = 5e-3
-#         time_step = 1e-3
-#         self._alpha = float(np.exp(-time_step / tau_syn))
-#         self._beta = float(np.exp(-time_step / tau_mem))
-
-    def get_trainable_parameters(self, lr):
-        res = [
-            {'params': self.w, 'lr': lr}, #, "weight_decay": DEFAULT_WEIGHT_DECAY},
-            {'params': self.b, 'lr': lr},
-            {'params': self.beta, 'lr': lr},
-        ]
-
-        if self.recurrent:
-            res.append({'params': self.v, 'lr': lr})
-        return res
-
-    def forward(self, x):
-        batch_size = x.shape[0]
-        nb_steps = x.shape[2]
-        
-        stride = tuple(self.stride)
-        padding = tuple(np.ceil(((self.kernel_size - 1) * self.dilation) / 2).astype(int))
-        conv_x = torch.nn.functional.conv3d(x, self.w, padding=padding,
-                                            dilation=tuple(self.dilation),
-                                            stride=stride)
-        conv_x = conv_x[:, :, :, :self.output_shape[0], :self.output_shape[1]]
-
-        mem = torch.zeros((batch_size, self.output_channels, *self.output_shape), dtype=x.dtype, device=x.device)
-        syn = torch.zeros((batch_size, self.output_channels, *self.output_shape), device=x.device, dtype=x.dtype)  # FIXME
-        spk = torch.zeros((batch_size, self.output_channels, *self.output_shape), dtype=x.dtype, device=x.device)
-
-        spk_rec = torch.zeros((batch_size, self.output_channels, nb_steps, *self.output_shape), dtype=x.dtype, device=x.device)
-        mem_rec = torch.zeros((batch_size, self.output_channels, nb_steps, *self.output_shape), dtype=x.dtype, device=x.device)
-
-        if self.lateral_connections:
-            d = torch.einsum("abcde, fbcde -> af", self.w, self.w)
-        b = self.b.unsqueeze(1).unsqueeze(1).repeat((1, *self.output_shape))
-
-        norm = (self.w ** 2).sum((1, 2, 3, 4))
-
-        for t in range(nb_steps):
-            # spike term
-            # mthr = torch.einsum("abcd,b->abcd", mem, 1. / (norm + self.eps)) - b
-            # spk = self.spike_fn(mthr)
-
-            if self.lateral_connections:
-                rst = torch.einsum("abcd,be ->aecd", spk, d)
-            else:
-                rst = torch.einsum("abcd,b,b->abcd", spk, self.b, norm)
-#                 rst = spk
-
-            input_ = conv_x[:, :, t, :, :]
-            if self.recurrent:
-                input_ = input_ + torch.einsum("abcd,be->aecd", spk, self.v)
-
-#             # TODO : check to see if this is actually works
-#             new_syn = self._alpha * syn + input_
-#             new_mem = self._beta * mem + syn - rst
-#             mem = new_mem
-#             syn = new_syn
-            mem = (mem - rst) * self.beta + input_ * (1. - self.beta)
-#             mthr = mem - b
-            mthr = torch.einsum("abcd,b->abcd", mem, 1. / (norm + self.eps)) - b
-            spk = self.spike_fn(mthr)
-
-            spk_rec[:, :, t, :, :] = spk
-            mem_rec[:, :, t, :, :] = mem
-            
-
-        self.spk_rec_hist = spk_rec.detach().cpu().numpy()
-        self.mem_rec_hist = mem_rec.detach().cpu().numpy()
-
-        if self.flatten_output:
-            output = torch.transpose(spk_rec, 1, 2).contiguous()
-            output = output.view(batch_size, nb_steps, self.output_channels * np.prod(self.output_shape))
-        else:
-            output = spk_rec
-
-        return output
-
-    def reset_parameters(self):
-        torch.nn.init.normal_(self.w, mean=self.w_init_mean,
-                              std=self.w_init_std * np.sqrt(1. / (self.input_channels * np.prod(self.kernel_size))))
-        if self.recurrent:
-            torch.nn.init.normal_(self.v, mean=self.w_init_mean,
-                                  std=self.w_init_std * np.sqrt(1. / self.output_channels))
-        torch.nn.init.normal_(self.beta, mean=0.7, std=0.01)
-        torch.nn.init.normal_(self.b, mean=1., std=0.01)
-
-    def clamp(self):
-        self.beta.data.clamp_(0., 1.)
-        self.b.data.clamp_(min=0.)
+# plt.bar(list(train_hist.keys()), list(train_hist.values()))
+# plt.title('Train Dataset Distribution')
+# plt.show()
+# plt.bar(list(test_hist.keys()), list(test_hist.values()))
+# plt.title('Test Dataset Distribution')
+# plt.show()
 
 
-# In[9]:
+# In[6]:
 
 
+def plot_one_batch(network):
+    for X_batch, _ in load_data('train'):
+        break
 
-class MyDense(torch.nn.Module):
-    IS_CONV = False
-    IS_SPIKING = True
-    HAS_PARAM = True
+    network.predict(X_batch)
 
-    def __init__(self, input_shape, output_shape, spike_fn, w_init_mean=W_INIT_MEAN, w_init_std=W_INIT_STD,
-                 recurrent=False, lateral_connections=True, eps=EPSILON):
-        super(MyDense, self).__init__()
-
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-        self.spike_fn = spike_fn
-        self.recurrent = recurrent
-        self.eps = eps
-        self.lateral_connections = lateral_connections
-
-        self.w_init_mean = w_init_mean
-        self.w_init_std = w_init_std
-
-        self.w = torch.nn.Parameter(torch.empty((input_shape, output_shape)), requires_grad=True)
-        if recurrent:
-            self.v = torch.nn.Parameter(torch.empty((output_shape, output_shape)), requires_grad=True)
-
-        self.beta = torch.nn.Parameter(torch.empty(1), requires_grad=True)
-        self.b = torch.nn.Parameter(torch.empty(output_shape), requires_grad=True)
-
-        self.reset_parameters()
-        self.clamp()
-        self.spk_rec_hist = None
-        self.mem_rec_hist = None
-        self.training = True
-
-    def get_trainable_parameters(self, lr):
-        res = [
-            {'params': self.w, 'lr': lr}, #, "weight_decay": DEFAULT_WEIGHT_DECAY},
-            {'params': self.b, 'lr': lr},
-            {'params': self.beta, 'lr': lr},
-        ]
-
-        if self.recurrent:
-            res.append({'params': self.v})
-        return res
-
-    def forward(self, x):
-        batch_size = x.shape[0]
-        nb_steps = x.shape[1]
-
-        h = torch.einsum("abc,cd->abd", x, self.w)
-
-        # membrane potential 
-        mem = torch.zeros((batch_size, self.output_shape), dtype=x.dtype, device=x.device)
-        spk = torch.zeros((batch_size, self.output_shape), dtype=x.dtype, device=x.device)
-
-        # output spikes recording
-        spk_rec = torch.zeros((batch_size, nb_steps, self.output_shape), dtype=x.dtype, device=x.device)
-        self.mem_rec_hist = torch.zeros((batch_size, nb_steps, self.output_shape), dtype=x.dtype, device=x.device)
-
-        if self.lateral_connections:
-            d = torch.einsum("ab, ac -> bc", self.w, self.w)
-
-        norm = (self.w ** 2).sum(0)
-
-        for t in range(nb_steps):
-            # reset term
-            if self.lateral_connections:
-                rst = torch.einsum("ab,bc ->ac", spk, d)
-            else:
-                rst = spk * self.b * norm
-
-            input_ = h[:, t, :]
-            if self.recurrent:
-                input_ = input_ + torch.einsum("ab,bc->ac", spk, self.v)
-
-            mem = (mem - rst) * self.beta + input_ * (1. - self.beta)
-            mthr = torch.einsum("ab,b->ab", mem, 1. / (norm + self.eps)) - self.b
-            spk = self.spike_fn(mthr)
-
-            spk_rec[:, t, :] = spk
-            self.mem_rec_hist[:, t, :] = mem
-
-            # save spk_rec for plotting
-        self.spk_rec_hist = spk_rec.detach().cpu().numpy()
-        self.mem_rec_hist = self.mem_rec_hist.detach().cpu().numpy()
-        return spk_rec
-
-    def reset_parameters(self):
-        torch.nn.init.normal_(self.w, mean=self.w_init_mean, std=self.w_init_std * np.sqrt(1. / self.input_shape))
-        if self.recurrent:
-            torch.nn.init.normal_(self.v, mean=self.w_init_mean, std=self.w_init_std * np.sqrt(1. / self.output_shape))
-        torch.nn.init.normal_(self.beta, mean=0.7, std=0.01)
-        torch.nn.init.normal_(self.b, mean=1., std=0.01)
-
-    def clamp(self):
-        self.beta.data.clamp_(0., 1.)
-        self.b.data.clamp_(min=0.)
-
+    for i,l in enumerate(network.layers):
+        if 'spk_rec_hist' in l.__dict__:
+            print("Layer {}: average number of spikes={:.4f}".format(i, l.spk_rec_hist.mean()))
+        plot_spikes_in_time(l)
 
 
 # In[10]:
-
-
-
-class MyReadout(torch.nn.Module):
-    IS_CONV = False
-    IS_SPIKING = False
-    HAS_PARAM = True
-
-    def __init__(self, input_shape, output_shape, w_init_mean=W_INIT_MEAN, w_init_std=W_INIT_STD, eps=EPSILON, time_reduction="mean"):
-
-        assert time_reduction in ["mean", "max"], 'time_reduction should be "mean" or "max"'
-
-        super(MyReadout, self).__init__()
-
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-
-        self.w_init_mean = w_init_mean
-        self.w_init_std = w_init_std
-
-        self.eps = eps
-        self.time_reduction = time_reduction
-
-        self.w = torch.nn.Parameter(torch.empty((input_shape, output_shape)), requires_grad=True)
-#         self.w = torch.nn.Parameter(torch.empty((10, 20)), requires_grad=True)
-        if time_reduction == "max":
-            self.beta = torch.nn.Parameter(torch.tensor(0.7 * np.ones((1))), requires_grad=True)
-        self.b = torch.nn.Parameter(torch.empty(output_shape), requires_grad=True)
-
-        self.reset_parameters()
-        self.clamp()
-
-        self.mem_rec_hist = None
-
-    def get_trainable_parameters(self, lr):
-        res = [
-            {'params': self.w, 'lr': lr}, #, "weight_decay": DEFAULT_WEIGHT_DECAY},
-            {'params': self.b, 'lr': lr},
-        ]
-
-        if self.time_reduction == "max":
-            res.append({'params': self.beta, 'lr': lr})
-        return res
-
-    def forward(self, x):
-        batch_size = x.shape[0]
-        h = torch.einsum("abc,cd->abd", x, self.w)
-        norm = (self.w ** 2).sum(0)
-
-        output = None
-        if self.time_reduction == "max":
-            nb_steps = x.shape[1]
-
-            mem = torch.zeros((batch_size, self.output_shape), dtype=x.dtype, device=x.device)
-            mem_rec = torch.zeros((batch_size, nb_steps, self.output_shape), dtype=x.dtype, device=x.device)
-
-            for t in range(nb_steps):
-                # membrane potential update
-                mem = mem * self.beta + (1 - self.beta) * h[:, t, :]
-                mem_rec[:, t, :] = mem
-
-            output = torch.max(mem_rec, 1)[0] / (norm + 1e-8) - self.b
-
-        elif self.time_reduction == "mean":
-            mem_rec = h
-            output = torch.mean(mem_rec, 1) / (norm + 1e-8) - self.b
-
-        # save mem_rec for plotting
-        self.mem_rec_hist = mem_rec.detach().cpu().numpy()
-        return output
-
-    def reset_parameters(self):
-        torch.nn.init.normal_(self.w, mean=self.w_init_mean, std=self.w_init_std * np.sqrt(1. / np.prod(self.input_shape)))
-        if self.time_reduction == "max":
-            torch.nn.init.normal_(self.beta, mean=0.7, std=0.01)
-        torch.nn.init.normal_(self.b, mean=1., std=0.01)
-
-    def clamp(self):
-        if self.time_reduction == "max":
-            self.beta.data.clamp_(0., 1.)
-
-
-
-# In[11]:
 
 
 network = SNN(device=device, dtype=dtype)
 network.time_expector = time_expector
 
 
-tau_mem = 10e-3
-tau_syn = 5e-3
-time_step = 1e-3
-beta = float(np.exp(-time_step / tau_mem))
-weight_scale = 7*(1.0 - beta) # =.6
+# tau_mem = 10e-3
+# tau_syn = 5e-3
+# time_step = 1e-3
+# beta = float(np.exp(-time_step / tau_mem))
+# weight_scale = 7*(1.0 - beta) # =.6
 
 # network.add_dense(
 #     input_shape=(64,64),
@@ -422,7 +185,7 @@ weight_scale = 7*(1.0 - beta) # =.6
 #     w_init_std=weight_scale
 # )
 
-network.add_layer(MyConv3D, #conv3d(
+network.add_conv3d(
     input_shape=(64,64),
     
     output_channels=128,
@@ -430,21 +193,21 @@ network.add_layer(MyConv3D, #conv3d(
     dilation=(1,1,1),
     lateral_connections=False,
     
-    w_init_mean=0.01,
+    w_init_mean=0.00,
     w_init_std=0.005
 )
 network.add_pool2d(kernel_size=(4,4))
 
-# network.add_dense(
-#     output_shape=128,
-#     w_init_mean=0.0,
-#     w_init_std=weight_scale
-# #     lateral_connections=True
-# )
+network.add_dense(
+    output_shape=128,
+    w_init_mean=0.0,
+    w_init_std=0.6
+#     lateral_connections=True
+)
 
-network.add_layer(MyReadout,#_readout(
+network.add_readout(
     output_shape=12,
-    time_reduction="max", # mean or max
+    time_reduction="max",
 
     w_init_mean=0.0,
     w_init_std=0.5
@@ -454,8 +217,7 @@ network.compile()
 network = network.to(network.device, network.dtype) # FIXME: this is a bug, fix it!
 
 
-# In[12]:
-
+# In[ ]:
 
 
 with open('results.log', 'w') as f:
@@ -470,7 +232,7 @@ with open('results.log', 'w') as f:
         result_file=f,
         save_checkpoints=True
     )
-    plot_metrics(res_metrics)
+    plot_metrics(res_metrics, save_plot_path='./metrics_' if SAVE_PLOTS else None)
 
 network.save('save_network.net')
 # network.load('save_network.net')
@@ -478,3 +240,11 @@ network.save('save_network.net')
 
 # In[ ]:
 
+
+plot_one_batch(network)
+print_and_plot_accuracy_metrics(
+    network, 
+    load_data('acc_train'), 
+    load_data('acc_test'), 
+    save_plot_path='./accuracy_' if SAVE_PLOTS else None
+)
