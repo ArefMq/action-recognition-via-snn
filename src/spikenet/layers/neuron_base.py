28f53b42a8c951e2d6a195472e41bac4961f969b
Aref Mehr <aref.mehr@boostinc.com>
2026-02-13 13:28:56 +0000

    refactor and improve neural base

--- src/spikenet/layers/neuron_base.py
+++ src/spikenet/layers/neuron_base.py
@@ -6,56 +6,58 @@ from spikenet.tools.configs import W_INIT_MEAN, W_INIT_STD
 
 
 class NeuronBase(torch.nn.Module, ABC):
-    """
-    Base class for all neuron layers used in the SpikeNet framework.
+    """Base class for all neuron layers used in the SpikeNet framework. This class is based on PyTorch's Module class,
+    and provides a simpler interface with spiking specific methods.
 
     Args:
-        name (str): Name of the layer (default: <id>_<neuron_type>)
+        name (str): Name of the layer
         in_features (int): Number of input features. None for getting the value from previous layer or input tensor.
         out_features (int): Number of output features.
         w_init_mean (float): Mean of the normal distribution used to initialize the weights.
         w_init_std (float): Standard deviation of the normal distribution used to initialize the weights.
     """
-    _id = 0
 
-    @classmethod
-    def __layer_id(cls) -> int:
-        """
-        Returns the unique id for the layer.
-        """
-        cls._id += 1
-        return cls._id
-
-    def __init__(self, **kwargs) -> None:
+    def __init__(
+        self,
+        name: str,
+        in_features: int,
+        out_features: int,
+        w_init_mean: float = W_INIT_MEAN,
+        w_init_std: float = W_INIT_STD,
+    ) -> None:
         super().__init__()
-        self.name = kwargs.get("name", f"{self.__layer_id()}_{self.__class__.__name__}")
-        self.in_features = kwargs["in_features"]
-        if self.in_features is not None:
-            self.in_features = int(self.in_features)
-        self.out_features = int(kwargs["out_features"])
-        self.w_init_mean = kwargs.get("w_init_mean", W_INIT_MEAN)
-        self.w_init_std = kwargs.get("w_init_std", W_INIT_STD)
+        self.name = name
+        self.in_features = in_features
+        self.out_features = out_features
+        self.w_init_mean = w_init_mean
+        self.w_init_std = w_init_std
 
     @property
     def params(self) -> list[torch.nn.Parameter]:
+        """Returns internal PyTorch parameters of this layer."""
         return [p for p in self.__dict__.values() if isinstance(p, torch.nn.Parameter)]
 
     @abstractmethod
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Runs the forward pass of the layer.
-        """
-        ...
+        """Runs the forward pass of the layer."""
 
     def initialize_parameters(self) -> None:
-        pass
+        """Initialise the parameters of the layer, usually with the internal mean and deviation"""
 
     def clamp(self) -> None:
-        pass
+        """Clamp the parameters of the layer to ensure they stay within valid ranges."""
 
     def reset(self) -> None:
+        """Reset the layer to its initial state."""
         self.initialize_parameters()
         self.clamp()
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~ STRING REPRESENTATION ~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def __repr__(self) -> str:
+        return (
+            f"{self.name}(in={self.in_features}, out={self.out_features}, "
+            f"mean={self.w_init_mean}, std={self.w_init_std})"
+        )
 
     def __str__(self) -> str:
         return f"{self.name}({self.out_features})"
