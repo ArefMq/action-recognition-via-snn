7aa2d002c39071224ca1a47f5067c06be25676d1
Aref Mehr <aref.mehr@boostinc.com>
2026-02-13 17:03:02 +0000

    clean flattening

--- src/spikenet/layers/flattening.py
+++ src/spikenet/layers/flattening.py
@@ -1,4 +1,6 @@
 import torch
+from torch import Tensor
+
 from spikenet.layers.neuron_base import NeuronBase
 
 
@@ -8,20 +10,17 @@ class Flatten(NeuronBase):
     This is used after the convolutional layers to flatten the output tensor to be
     used by the dense layer.
     """
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        (batch_size, _, nb_steps, *_) = x.shape
-        output = torch.transpose(x, 1, 2).contiguous()
-        output = output.view(batch_size, nb_steps, self.out_features)
-        return output
+    def forward(self, spk_rec: Tensor) -> Tensor:
+        """
+        Forward pass of the flatten layer.
 
-    def plot_mem(*args, **kwargs) -> None:
-        # Flatten layer does not have membrane potential
-        pass
+        Args:
+            spk_rec (Tensor): The spike record tensor with shape (batch_size, channels, nb_steps, *spatial_dims)
 
-    def plot_spk(*args, **kwargs) -> None:
-        # Flatten layer does not have spikes
-        pass
-        
+        Returns:
+            Tensor: The flattened tensor with shape (batch_size, nb_steps, flattened_features)
+        """
+        (batch_size, nb_channels, nb_steps, *_) = spk_rec.shape
+        output = torch.transpose(spk_rec, 1, 2).contiguous()
+        return output.view(batch_size, nb_steps, self.out_features)
